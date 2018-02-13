226b4e99bdb9b9670b791d34d9bbc8e907eb7aea
James Coyle <james.coyle@jamescoyle.net>
2018-02-13 16:30:44 +0000

    Further development - not in working state

--- storjalytics/storjalytics_send.py
+++ storjalytics/storjalytics_send.py
@@ -12,23 +12,52 @@ from os import scandir
 ### Vars
 CONFIGFILE = '/etc/storjalytics/config.json'
 APIENDPOINT = 'http://localhost:8080/api/'
-PARSER = None
+APIKEY = None
+APISECRET = None
+SERVERGUID = None
+STORJCONFIG = None
 
 def init_send():
-    global APIENDPOINT
-    global PARSER
 
-    cmdargs()
+    load_settings()
+
+    storj_cmd = storjalytics_common.check_strojshare()
+
+    result = subprocess_result(['storjshare', 'status'])
+
+    configs = os.scandir(STORJCONFIG)
+
+def load_settings():
+    global CONFIGFILE
+    global APIKEY
+    global APISECRET
+    global SERVERGUID
+    global STORJCONFIG
+
+    try:
+        settings_file = open(CONFIGFILE, 'r')
+        settings_data = settings_file.read()
+        settings_json = json.loads(settings_data)
+
+        APIKEY = settings['api_key']
+        APISECRET = settings['api_secret']
+        SERVERGUID = settings['server_guid']
+        STORJCONFIG = settings['storj_config']
+
+    except KeyError:
+        error_message('Settings file ' + CONFIGFILE + ' invalid. Please check your config.')
+    except json.JSONDecodeError:
+        error_message('Settings file ' + CONFIGFILE + ' invalid. Please check your config.')
+    except FileNotFoundError:
+        error_message('Settings file ' + CONFIGFILE + ' not found. Please run storjalytics-register.')
 
 
-def cmdargs():
-    global PARSER
+def print_error(error_message):
+    print()
+    print("ERROR : " + error_message)
+    print()
 
-    PARSER = argparse.ArgumentParser(prog='storjalytics-register', add_help=True)
-    PARSER.add_argument('--email', '-e', help="Your StorjAlytics registered email address", type=str, action='store', dest='arg_email', nargs='?')
-    PARSER.add_argument('--password', '-p', help="Your StorjAlytics password", type=str, action='store', dest='arg_password', nargs='?')
-    PARSER.add_argument('--description', '-d', help="Description for this server", type=str, action='store', dest='arg_desc', nargs='?')
-    PARSER.add_argument('--config-dir', '-c', help="The location to your local Storjshare config file", type=str, action='store', dest='arg_config_dir', nargs='?')
+    exit(1)
 
 if __name__ == '__main__':
     init_send()
