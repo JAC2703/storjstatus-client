cfd7b2572ed4d1e50a5a4a14dfb2a8dca7aae5f7
James Coyle <james.coyle@jamescoyle.net>
2018-03-06 11:42:31 +0000

    Dependencies are now in requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_packages
+from setuptools import setup
+from pip.req import parse_requirements
 
 exec(open('storjstatus/version.py').read())
 
+dependencies = parse_requirements('requirements.txt', session=False)
+
 setup(name='storjstatus',
       version=__version__,
-      description='Storj utility for reporting farmer statistics',
+      description='A utility for reporting Storj farmer statistics',
       license='MIT',
       packages=['storjstatus'],
       author='James Coyle',
       author_email='james.coyle@jamesdcoyle.net',
       python_requires='>=3.5',
       url='https://www.storjstatus.com/install-client',
-      packages=find_packages(),
+      install_requires=[str(ir.req) for ir in dependencies],
       entry_points={
           'console_scripts': [
               'storjstatus-register=storjstatus:register',
