549c33f7fd50ef93c4daf3a87852ee841c8ea096
James Coyle <james.coyle@jamescoyle.net>
2018-03-06 13:19:06 +0000

    Update for long_description

--- setup.py
+++ setup.py
@@ -6,25 +6,24 @@ import pypandoc
 
 exec(open('storjstatus/version.py').read())
 
-dependencies = parse_requirements('requirements.txt', session=False)
+def dependencies():
+    return parse_requirements('requirements.txt', session=False)
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+def readme():
+    with open('README.md') as f:
+        return f.read()
 
 setup(name='storjstatus',
       version=__version__,
       description='A utility for reporting Storj farmer statistics',
-      long_description=long_description,
+      long_description=readme(),
       license='MIT',
       packages=['storjstatus'],
       author='James Coyle',
       author_email='james.coyle@jamesdcoyle.net',
       python_requires='>=3.5',
       url='https://www.storjstatus.com/install-client',
-      install_requires=[str(ir.req) for ir in dependencies],
+      install_requires=[str(ir.req) for ir in dependencies()],
       entry_points={
           'console_scripts': [
               'storjstatus-register=storjstatus:register',
